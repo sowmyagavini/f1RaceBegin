2d851dbcf175dc55c8606d3bd0338f53f0b73999
sowmyagavini <sowmyagavini0023@gmail.com>
2021-03-24 12:33:24 +0530

    F1 Race begins in...

---
+++ src/f1RaceBegin/F1RaceBegins.java
@@ -0,0 +1,20 @@
+package f1RaceBegin;
+
+import java.util.Scanner;
+
+public class F1RaceBegins {
+
+	public static void main(String[] args) throws InterruptedException {
+System.out.println("3");
+Thread.sleep(1000);
+System.out.println("2");
+Thread.sleep(1000);
+
+System.out.println("1");
+Thread.sleep(1000);
+
+System.out.println("AND GOOOOOOOOOOO!!!! Vroom Vrooooom....");
+
+	}
+
+}
